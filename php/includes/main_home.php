ae94861921d9a201ccd9d2709b3db6ac037e05ea
Aaron Varga <apvarga@student.fullsail.edu>
2022-04-18 23:27:10 -0400

    updated styling and content

---
+++ php/includes/main_home.php
@@ -0,0 +1,48 @@
+<main>
+  <div class="container">
+    <section>
+      <h2><span>#</span> About Me <span><i class="fa-solid fa-arrow-turn-down"></i></span></h2>
+      <p>I have earned a BA in Digital Media Design and I am currently finishing a BS in Web Development.</p>
+      <p>I grew up first designing user interfaces and later on I dove into the development side of things as well. I love the LAMP stack! I also have experience with the MERN stack.</p>
+      <p>I am a UI Designer and Full Stack Developer who loves to use super clean vanilla code unless other technologies are absolutely necessary. My favorite coding languages and technologies are HTML, CSS/LESS, JavaScript, PHP, and MySQL.</p>
+    </section>
+    <section>
+      <h2><span>#</span> My Stack <span><i class="fa-solid fa-arrow-turn-down"></i></span></h2>
+      <ul>
+        <li>Linux OS</li>
+        <li>Apache Web Server</li>
+        <li>MySQL Database</li>
+        <li>PHP</li>
+        <li>JavaScript</li>
+        <li>CSS/LESS</li>
+        <li>HTML</li>
+      </ul>
+    </section>
+    <section>
+      <h2><span>#</span> Recent Projects <span><i class="fa-solid fa-arrow-turn-down"></i></span></h2>
+      <div class="recent-projects-grid">
+        <div class="recent-projects-grid-item">
+          Item
+        </div>
+        <div class="projects-grid-item">
+          Item
+        </div>
+      </div>
+    </section>
+    <section>
+      <h2><span>#</span> Recent Blogs <span><i class="fa-solid fa-arrow-turn-down"></i></span></h2>
+      <div class="recent-blogs-grid">
+        <div class="recent-blogs-grid-item">
+          Item
+        </div>
+        <div class="projects-grid-item">
+          Item
+        </div>
+      </div>
+    </section>
+    <section>
+      <h2><span>#</span> Hire Me <span><i class="fa-solid fa-arrow-turn-down"></i></span></h2>
+      <p>Link for contact form goes here...</p>
+    </section>
+  </div>
+</main>
