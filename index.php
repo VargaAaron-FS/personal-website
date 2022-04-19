76266fa95fba2d4c48c032de204be4bd4546feae
Aaron Varga <apvarga@student.fullsail.edu>
2022-04-19 16:36:43 -0400

    formatting updates

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta
-      name="description"
-      content="Web site created by Aaron Varga"
-    />
+    <meta name="description" content="Web site created by Aaron Varga" />
     <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
@@ -17,10 +15,15 @@
     <title>Aaron Varga | UI Designer + Full Stack Developer</title>
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  </head>
-  <body>
-    <script> // Empty for bug fix </script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <script>
+    // Empty for bug fix 
+    </script>
     <noscript>You need to enable JavaScript to interact with this page.</noscript>
     <!-- Header -->
     <?php include "./php/includes/header.php" ?>
@@ -30,6 +33,7 @@
     <?php include "./php/includes/main_home.php" ?>
     <!-- Footer -->
     <?php include "./php/includes/footer.php" ?>
-  </body>
-  <script type="text/javascript" src="./js/script.js" defer></script>
+</body>
+<script type="text/javascript" src="./js/script.js" defer></script>
+
 </html>
--- php/includes/footer.php
+++ php/includes/footer.php
@@ -1,10 +1,10 @@
 <footer>
-  <div class="container">
-    <div class="footer-icons-wrapper">
-      <a href="#"><i class="fa-brands fa-dribbble"></i></a>
-      <a href="#"><i class="fa-brands fa-github"></i></a>
-      <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+    <div class="container">
+        <div class="footer-icons-wrapper">
+            <a href="#"><i class="fa-brands fa-dribbble"></i></a>
+            <a href="#"><i class="fa-brands fa-github"></i></a>
+            <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+        </div>
+        <p>&copy;&nbsp;Aaron&nbsp;Varga&nbsp;2022. All&nbsp;rights&nbsp;reserved.</p>
     </div>
-    <p>&copy;&nbsp;Aaron&nbsp;Varga&nbsp;2022. All&nbsp;rights&nbsp;reserved.</p>
-  </div>
 </footer>
