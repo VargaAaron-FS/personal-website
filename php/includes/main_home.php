6ddf050d6a680c9f8123b6451d9a117c69a11f32
Aaron Varga <apvarga@student.fullsail.edu>
2022-04-20 16:31:56 -0400

    added project image placeholder and updated styles

--- php/includes/main_home.php
+++ php/includes/main_home.php
@@ -18,24 +18,29 @@
                 <div class="flex-item">
                     <h3><span><i class="fa-solid fa-layer-group"></i></span> LAMP</h3>
                     <ul>
-                        <li><a href="#">Linux</a></li>
-                        <li><a href="#">Apache</a></li>
-                        <li><a href="#">MySQL</a></li>
-                        <li><a href="#">PHP</a></li>
-                        <li><a href="#">JavaScript</a></li>
-                        <li><a href="#">CSS</a>/<a href="#">LESS</a></li>
-                        <li><a href="#">HTML</a></li>
+                        <li><a href="https://www.linux.org/" target="_blank">Linux</a></li>
+                        <li><a href="https://httpd.apache.org/" target="_blank">Apache</a></li>
+                        <li><a href="https://www.mysql.com/" target="_blank">MySQL</a></li>
+                        <li><a href="https://www.php.net/" target="_blank">PHP</a></li>
+                        <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+                                target="_blank">JavaScript</a></li>
+                        <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">CSS</a>/<a
+                                href="https://lesscss.org/" target="_blank">LESS</a></li>
+                        <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics"
+                                target="_blank">HTML</a></li>
                     </ul>
                 </div>
                 <div class="flex-item">
                     <h3><span><i class="fa-solid fa-layer-group"></i></span> MERN</h3>
                     <ul>
-                        <li><a href="#">MongoDB</a></li>
-                        <li><a href="#">Express JS</a></li>
-                        <li><a href="#">React JS</a></li>
-                        <li><a href="#">Node JS</a></li>
-                        <li><a href="#">CSS</a>/<a href="#">LESS</a></li>
-                        <li><a href="#">HTML</a></li>
+                        <li><a href="https://www.mongodb.com/" target="_blank">MongoDB</a></li>
+                        <li><a href="https://expressjs.com/" target="_blank">Express JS</a></li>
+                        <li><a href="https://reactjs.org/" target="_blank">React JS</a></li>
+                        <li><a href="https://nodejs.org/" target="_blank">Node JS</a></li>
+                        <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank">CSS</a>/<a
+                                href="https://lesscss.org/" target="_blank">LESS</a></li>
+                        <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/HTML_basics"
+                                target="_blank">HTML</a></li>
                     </ul>
                 </div>
             </div>
@@ -44,27 +49,30 @@
             <h2><span>#</span> My Tools <span><i class="fa-solid fa-arrow-turn-down"></i></span></h2>
             <div class="flexbox">
                 <div class="flex-item">
-                    <h3><span><i class="fa-solid fa-compass-drafting"></i></span> Design</h3>
+                    <h3><span><i class="fa-solid fa-compass-drafting"></i></span>Design</h3>
                     <ul>
-                        <li><a href="#">Adobe XD</a></li>
-                        <li><a href="#">Adobe Photoshop</a></li>
-                        <li><a href="#">Adobe Illustrator</a></li>
-                        <li><a href="#">Adobe After Effects</a></li>
-                        <li><a href="#">Adobe Dimensions</a></li>
-                        <li><a href="#">Adobe Animate</a></li>
+                        <li><a href="https://www.adobe.com/products/xd.html" target="_blank">Adobe XD</a></li>
+                        <li><a href="https://www.adobe.com/products/photoshop.html" target="_blank">Adobe Photoshop</a>
+                        </li>
+                        <li><a href="https://www.adobe.com/products/illustrator.html" target="_blank">Adobe
+                                Illustrator</a></li>
+                        <li><a href="https://www.adobe.com/products/aftereffects.html" target="_blank">Adobe After
+                                Effects</a></li>
+                        <li><a href="https://www.adobe.com/products/dimension.html" target="_blank">Adobe Dimensions</a>
+                        </li>
+                        <li><a href="https://www.adobe.com/products/animate.html" target="_blank">Adobe Animate</a></li>
                     </ul>
                 </div>
                 <div class="flex-item">
-                    <h3><span><i class="fa-solid fa-code"></i></span> Development</h3>
+                    <h3><span><i class="fa-solid fa-code"></i></span>Development</h3>
                     <ul>
-                        <li><a href="#">Terminal</a></li>
-                        <li><a href="#">Git</a>/<a href="#">GitHub</a></li>
-                        <li><a href="#">MAMP</a></li>
-                        <li><a href="#">VS Code</a></li>
-                        <li><a href="#">CodeKit</a></li>
-                        <li><a href="#">MySQL Workbench</a></li>
-                        <li><a href="#">Postman</a></li>
-                        <li><a href="#">Transmit</a></li>
+                        <li><a href="https://git-scm.com/" target="_blank">Git</a>/<a href="https://github.com/"
+                                target="_blank">GitHub</a></li>
+                        <li><a href="https://www.mamp.info/" target="_blank">MAMP</a></li>
+                        <li><a href="https://code.visualstudio.com/" target="_blank">VS Code</a></li>
+                        <li><a href="https://codekitapp.com/" target="_blank">CodeKit</a></li>
+                        <li><a href="https://www.mysql.com/products/workbench/" target="_blank">MySQL Workbench</a></li>
+                        <li><a href="https://www.postman.com/" target="_blank">Postman</a></li>
                     </ul>
                 </div>
             </div>
@@ -72,26 +80,22 @@
         <section>
             <h2><span>#</span> Recent Projects <span><i class="fa-solid fa-arrow-turn-down"></i></span></h2>
             <div class="recent-projects-grid">
-                <div class="recent-projects-grid-item">
-                    <img src="" alt="Project Image">
-                    <h3>Project Title</h3>
-                    <p>Description</p>
-                </div>
-                <div class="recent-projects-grid-item">
-                    <img src="" alt="Project Image">
-                    <h3>Project Title</h3>
-                    <p>Description</p>
-                </div>
-                <div class="recent-projects-grid-item">
-                    <img src="" alt="Project Image">
-                    <h3>Project Title</h3>
-                    <p>Description</p>
-                </div>
-                <div class="recent-projects-grid-item">
-                    <img src="" alt="Project Image">
-                    <h3>Project Title</h3>
-                    <p>Description</p>
-                </div>
+                <a href="#" class="recent-projects-grid-item">
+                    <img src="./images/portfolio-project-thumbnail.jpg" alt="Project Image">
+                    <h3>Varga Lujos<span><i class="fa-solid fa-rocket"></i></span></h3>
+                </a>
+                <a href="#" class="recent-projects-grid-item">
+                    <img src="./images/portfolio-project-thumbnail.jpg" alt="Project Image">
+                    <h3>Project Title<span><i class="fa-solid fa-rocket"></i></span></h3>
+                </a>
+                <a href="#" class="recent-projects-grid-item">
+                    <img src="./images/portfolio-project-thumbnail.jpg" alt="Project Image">
+                    <h3>Project Title<span><i class="fa-solid fa-rocket"></i></span></h3>
+                </a>
+                <a href="#" class="recent-projects-grid-item">
+                    <img src="./images/portfolio-project-thumbnail.jpg" alt="Project Image">
+                    <h3>Project Title<span><i class="fa-solid fa-rocket"></i></span></h3>
+                </a>
             </div>
             <button class="primary_button">View All Projects</button>
         </section>
